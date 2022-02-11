b7b825cd02e184195e2c19a7da8ae689fa943ebe
Andrij-Maksymyshyn <84930890+Andrij-Maksymyshyn@users.noreply.github.com>
2022-02-11 21:30:03 +0200

    catch error

--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -10,52 +10,42 @@ const noPosterImg = 'https://sd.keepcalms.com/i/sorry-no-picture-available-2.png
 export function MovieDetailsPage() {
     const { moviesId } = useParams();
     const [movie, setMovie] = useState(null);
-    // const [errorM, setErrorM] = useState(null);
     const [loading, setLoading] = useState(false);
 
 
     useEffect(() => {
-               setLoading(true);
+               setLoading(true);      
 
-        try {
-            fetchMovieById(moviesId).then(data => {
-                const { data: { poster_path, title, name, overview, vote_average, genres } } = data;
-                if (!overview) {
-                return toast.error('Sorry, there is no film. Try another request...');
-                };
-                                 
-                setMovie({
-                    poster: poster_path ? ('https://image.tmdb.org/t/p/w500' + poster_path) : noPosterImg,
-                    title,
-                    name,
-                    overview,
-                    vote_average,
-                    genresValues: (genres.length === 0) ?
-                        "There are no genres" : 
-                        (genres.map(({ name }) => (
-                        [name]
-                        )).join(", "))
-                        });              
-
-            })      
-
-        }
-        catch (error) {
-            // setErrorM(error);
-            console.log('OOOps EEEEEEEError');
-            throw error;
-            
-            
-        }
-        finally {
-           
-      setTimeout(() => {
-          setLoading(false);           
-      }, 1000);
-        } 
         
-    }, [moviesId]);    
-   
+        fetchMovieById(moviesId).then(data => {
+            const { data: { poster_path, title, name, overview, vote_average, genres } } = data;                
+               
+                                               
+            setMovie({
+                poster: poster_path ? ('https://image.tmdb.org/t/p/w500' + poster_path) : noPosterImg,
+                title,
+                name,
+                overview,
+                vote_average,
+                genresValues: (genres.length === 0) ?
+                    "There are no genres" :
+                    (genres.map(({ name }) => (
+                        [name]
+                    )).join(", "))
+            });
+
+        })
+            
+            .catch(error => {
+                console.log('Whoops, something went wrong...', error);
+                return toast.error('Sorry, there is no film. Try another request...');
+            })
+
+            .finally(() => setTimeout(() => {
+                setLoading(false);
+            }, 1000));       
+        
+    }, [moviesId]);      
     
    
 
@@ -82,7 +72,7 @@ export function MovieDetailsPage() {
                 </Box>                    
                 )}              
                                 
-            {/* {errorM && <h2>Whoops, something went wrong: error.</h2>} */}
+            
         </Main>
     )
 };
